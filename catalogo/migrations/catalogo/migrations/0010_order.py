6600a795fe150214484d466dcf109c8b13a2616f
Espaik2020 <ec757602@gmail.com>
2025-11-11 10:21:14 -0600

    0010_order.py

---
+++ catalogo/migrations/catalogo/migrations/0010_order.py
@@ -0,0 +1,30 @@
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalogo', '0009_alter_newslettersubscriber_email'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
+                ('items', models.JSONField(default=list)),
+                ('status', models.CharField(choices=[('CREATED', 'Creado'), ('PAID', 'Pagado (simulado)'), ('CANCELLED', 'Cancelado')], default='CREATED', max_length=20)),
+                ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': 'Pedido',
+                'verbose_name_plural': 'Pedidos',
+                'ordering': ['-created_at'],
+            },
+        ),
+    ]
